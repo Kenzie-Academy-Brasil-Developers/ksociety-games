4662fce8a5d7453a62e39987fa8b8e8750f1430e
occasum13 <daybreak98@hotmail.com>
2021-11-23 17:37:28 -0300

    first desk

---
+++ script.js
@@ -0,0 +1,36 @@
+@import url('https://fonts.googleapis.com/css2?family=Inter&family=Roboto+Mono&display=swap');
+
+
+
+body {
+
+    background-color: black;
+    color: #41C93E;
+    font-family:  'Roboto Mono', monospace;
+    width: 100%;
+    height: 1090px;
+}
+
+header {
+
+    display: flex;
+    width: auto;
+    justify-content: center;
+    height: 50px;
+
+}
+
+h3 {
+    display: flex;
+    align-items: center;
+}
+
+section {
+    
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    justify-content: center;
+
+}
