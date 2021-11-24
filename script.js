3ad54ed4af432faa4eeb0538bd31f465d385250d
Brendo Sousa <brendo.n.s.sousa@gmail.com>
2021-11-24 15:01:37 -0300

    Criação do arquivo js para o jogo Caça-Níquel

--- script.js
+++ script.js
@@ -28,47 +28,6 @@ function criaTabela(){
 
 */
 
-// CRIA A TABELA NO HTML E ADICIONA AS LETRAS ALEATÓRIAS NAS POSIÇÕES DA TABELA
-
-
-// CASSINO
-
-const numeros = [1,2,3,4,5,6,7,8,9]
-let count = 0;
-
-function slot(){
-    const random = setInterval(function(){
-        count++
-
-        let esquerda = Math.floor(Math.random() *10)
-        let centro = Math.floor(Math.random() *10)
-        let direita = Math.floor(Math.random() *10)
-        //
-        let slotMeio = document.getElementById('slot-esqueda')
-        slotMeio.firstElementChild.innerHTML = esquerda
-        let slotCentro = document.getElementById('slot-centro')
-        slotCentro.firstElementChild.innerHTML = centro
-        let slotDireita = document.getElementById('slot-direita')
-        slotDireita.firstElementChild.innerHTML = direita
-
-        if(count > 20){
-            let finalEsquerda = numeros[esquerda]
-            let finalCentro = numeros[centro]
-            let finalDireita = numeros[direita]
-
-            if((finalEsquerda===finalDireita)&&(finalEsquerda===finalCentro)&&(finalDireita===finalCentro)){
-                document.getElementById('result').innerText = 'Você acertou as 3 posições e ganhou!'
-            }
-            else if((finalEsquerda===finalCentro) || (finalEsquerda===finalDireita) || (finalDireita===finalCentro)){
-                document.getElementById('result').innerText = 'Você acertou 2 posições e ganhou metade do premio!'
-            }else{
-                document.getElementById('result').innerText = 'Você perdeu, tente novamente'
-            }
-            count = 0;
-            clearInterval(random)
-        }
-    },100)
-}
 let matrix = [];
 
 //Cria uma matriz 10x10 
@@ -212,8 +171,6 @@ function pesquisaPalavra(){
                 counter = 0;
             }
         }
-        
-        //console.log(palavrasDaMatrix[posicao]);
         
         console.log(palavrasDaMatrix);
     })
