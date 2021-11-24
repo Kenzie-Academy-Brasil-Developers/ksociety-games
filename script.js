0947ec364a2349ff53efc66f185a7a91001d50a2
Brendo Sousa <brendo.n.s.sousa@gmail.com>
2021-11-24 20:14:49 -0300

    Ajustes de bugs

--- script.js
+++ script.js
@@ -177,6 +177,7 @@ function pesquisaPalavra(){
         console.log(posicao);
         if(palavrasDaMatrix.includes(search.toUpperCase())){
             palavrasDaMatrix.splice(posicao, 1);
+            entrada.value = "";
             counter++;  
             if(counter < 3){
                 alert("Parabéns, achou uma palavra!");
