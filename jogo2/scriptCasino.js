6a8b1415f603e287b2482e56eb5d2b7f238de148
Brendo Sousa <brendo.n.s.sousa@gmail.com>
2021-11-24 22:28:13 -0300

    Adição do placar no jogo Caça-Níquel

--- jogo2/scriptCasino.js
+++ jogo2/scriptCasino.js
@@ -2,10 +2,16 @@
 
 const numeros = [1,2,3,4,5,6,7,8,9]
 let count = 0;
+let vitorias = 0;
+let derrotas = 0;
 
 function slot(){
     const cassino = setInterval(function(){
         count++
+
+        let mostraVitorais = document.getElementById('vitorias');
+        let mostraDerrotas = document.getElementById('derrotas');
+
 
         let esquerda = Math.floor(Math.random() *9)
         let centro = Math.floor(Math.random() *9)
@@ -28,11 +34,17 @@ function slot(){
 
             if((finalEsquerda===finalDireita)&&(finalEsquerda===finalCentro)&&(finalDireita===finalCentro)){
                 document.getElementById('result').innerText = 'Você acertou as 3 posições e ganhou!'
+                vitorias++;
+                mostraVitorais.innerText = `Vitórias: ${vitorias}`
             }
             else if((finalEsquerda===finalCentro) || (finalEsquerda===finalDireita) || (finalDireita===finalCentro)){
                 document.getElementById('result').innerText = 'Você acertou 2 posições e ganhou metade do premio!'
+                derrotas++;
+                mostraDerrotas.innerText = `Derrotas: ${derrotas}`
             }else{
                 document.getElementById('result').innerText = 'Você perdeu, tente novamente'
+                derrotas++;
+                mostraDerrotas.innerText = `Derrotas: ${derrotas}`
             }
             count = 0;
             clearInterval(cassino)
